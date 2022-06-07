45a20159dd7bd1e28a47f8ff2ac4e897760ae593
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-06-07 16:22:37 +0200

    changed toml file

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e395f3038b18451"; }
+/******/ 		__webpack_require__.h = function() { return "2528209fc9b0176e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
