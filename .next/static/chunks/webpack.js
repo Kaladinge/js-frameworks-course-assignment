84d40554b9e0981b52bbb634f1cfae07e782d1ff
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-06-08 10:10:13 +0200

    added bootstrap css via CDN instead of import method

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0f78baffd7b2bac6"; }
+/******/ 		__webpack_require__.h = function() { return "9c2bbbdbaed25793"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
