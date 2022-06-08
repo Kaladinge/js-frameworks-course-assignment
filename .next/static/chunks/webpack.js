0b3c7f543549894b2ec4b4850da5af188fd55c1b
Kaladinge <62391604+Kaladinge@users.noreply.github.com>
2022-06-08 10:01:01 +0200

    open

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0378f2875c1f059b"; }
+/******/ 		__webpack_require__.h = function() { return "0f78baffd7b2bac6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
